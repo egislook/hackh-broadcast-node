a90e61ee6730f5c62882ea405333358f48520c44
blazblack <hengsovandara15@gmail.com>
2020-04-04 13:19:52 +0700

    update

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
 module.exports.debug                 = false;
 module.exports.reSignUp              = true;
 module.exports.env                   = process.env.ENV                                      || 'test';
-
-module.exports.MEKONG_SMS_ENDPOINT   = process.env.MEKONG_SMS_ENDPOINT                      || 'https://mekongsms.clik.asia/api/sendsms.aspx';
-module.exports.MEKONG_SMS_USER       = process.env.MEKONG_SMS_USER                          || 'clikpayment@mekongnet';
-module.exports.MEKONG_SMS_HASH       = process.env.MEKONG_SMS_HASH                          || '2C9FD9C00DBAB083E1FBADD222D1535A';
-module.exports.MEKONG_SMS_SENDER     = process.env.MEKONG_SMS_SENDER                        || 'Clik';
