264ec52a8b5d69e41cdd14cde13173bf0685d4c6
EgisLook <noneedsystem@gmail.com>
2020-04-04 17:08:44 +0700

    fix: changed to different twilio credentials

--- functions/Auth/index.js
+++ functions/Auth/index.js
@@ -1,9 +1,9 @@
 const { fail, success, extract } = require('../utils')
 const { firebaseAuthRegister, firebaseVerify } = require('./firebase')
 
-const TWILIO_SID = 'ACe0795b2cb3bff67717e01c6a94b12d68'
-const TWILIO_TOKEN = '11fa748e9e449d37f8438d22fba4b9d1'
-const TWILIO_FROM = '+12029157522'
+const TWILIO_SID = 'AC9af09c4f76d8c7762ebee7680ead372f' || 'ACe0795b2cb3bff67717e01c6a94b12d68'
+const TWILIO_TOKEN = '3f3ea60fa7b8eb601ec12c5eb034104d' || '11fa748e9e449d37f8438d22fba4b9d1'
+const TWILIO_FROM = '+18506331535' || '+12029157522'
 
 module.exports.handler = async event => {
   const { body } = extract(event)
