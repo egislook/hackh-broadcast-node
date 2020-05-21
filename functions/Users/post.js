710f389c5df98af6aed5f9188ebd8634e22f2b55
Mike Van <miike.vaan@gmail.com>
2020-05-21 17:34:11 +0700

    update user POST

--- functions/Users/post.js
+++ functions/Users/post.js
@@ -10,13 +10,13 @@ module.exports.handler = async event => {
 
     let auth = await firebaseCheckAuth(token)
     if (!auth) return fail({ message: 'Unauthorized access' })
-    
-    if (!displayName && !phoneNumber) return fail({ message: 'you need provide displayName and phone.' })
-    
+
     if (uid){
-      result = await updateUser({ uid, displayName, role, photoUrl})
+      result = await updateUser({ uid, displayName, role, photoUrl })
       return success(result)
     }
+
+    if (!displayName && !phoneNumber) return fail({ message: 'you need provide displayName and phone.' })
 
     uid = phoneNumber.replace(/^\+855|^0/, '855')
     // const user = await firebaseCheckUser(uid)
@@ -43,14 +43,14 @@ module.exports.handler = async event => {
   }
 }
 
-const updateUser = async ({ uid, displayName}) => {
+const updateUser = async ({ uid, displayName, role, photoUrl }) => {
   try {
-    return admin.auth().updateUser(uid, { displayName })
+    await admin.auth().setCustomUserClaims(uid, { role })
+    return admin.auth().updateUser(uid, { displayName, photoUrl })
       .then(async function (userRecord) {
         // See the UserRecord reference doc for the contents of userRecord.
         const user = await userRecord.toJSON() || {}
-
-        return { uid: uid, displayName, phoneNumber: user.phoneNumber, role: user.customClaims.role, photoUrl: user.photoUrl }
+        return { uid: uid, displayName, phoneNumber: user.phoneNumber, role: user.customClaims.role, photoUrl }
       })
       .catch(function (error) {
         throw error
@@ -60,4 +60,3 @@ const updateUser = async ({ uid, displayName}) => {
   }
 
 }
-
